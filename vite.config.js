b08cdee914d6f63208020d10e612dbdbb4789b65
Farzane <farzane.njf.1377@gmail.com>
2024-09-30 21:48:58 +0330

    absolute imports

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,21 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+
+  // absolute imports
+  resolve : {
+    alias : {
+      "@src" : "/src",
+      "@app" : "/src/app/",
+      "@assets" : "/src/assets",
+      "@components" : "/src/components",
+      "@core" : "/src/core",
+      "@config" : "/src/config",
+      "@screens" : "/src/screens",
+      "@store": "/src/store",
+    }
+  }
 })
+
+
